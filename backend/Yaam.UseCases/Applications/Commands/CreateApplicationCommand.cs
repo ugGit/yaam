e2e803f0eaac4fae2cdefd2f2df31f140ab206b2
Uchendu Nwachukwu <uchendu.nwachukwu@myky.ch>
2026-07-16 11:33:35 +0200

    fix: remove redundant using in CreateApplicationCommand

--- backend/Yaam.UseCases/Applications/Commands/CreateApplicationCommand.cs
+++ backend/Yaam.UseCases/Applications/Commands/CreateApplicationCommand.cs
@@ -3,7 +3,6 @@ using MediatR;
 using Yaam.Domain.Entities;
 using Yaam.Domain.Enums;
 using Yaam.Domain.Repositories;
-using Yaam.UseCases.Applications;
 using Yaam.UseCases.Applications.Dtos;
 
 namespace Yaam.UseCases.Applications.Commands;
