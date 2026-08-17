f84a264e283e59238140c510cf8594b7e4f12e84
Uchendu Nwachukwu <uchendu.nwachukwu@myky.ch>
2026-08-17 22:13:27 +0200

    fix(cv-settings): update default OllamaModel to llama3.2

--- backend/Yaam.UseCases/Common/Cv/CvSettings.cs
+++ backend/Yaam.UseCases/Common/Cv/CvSettings.cs
@@ -3,6 +3,6 @@ namespace Yaam.UseCases.Common.Cv;
 public class CvSettings
 {
     public string OllamaBaseUrl { get; init; } = "http://localhost:11434";
-    public string OllamaModel { get; init; } = "llama3.1";
+    public string OllamaModel { get; init; } = "llama3.2";
     public int TimeoutSeconds { get; init; } = 300;
 }
